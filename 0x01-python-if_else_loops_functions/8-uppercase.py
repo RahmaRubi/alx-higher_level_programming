9cb2053df289e9d230f11fb2e02241ccd8af015c
RahmaRubi <rahmaelroby47@gmail.com>
2023-08-09 06:42:46 -0700

    done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-        if (not (str.isupper(char))):
-            pass
-
-        print("{:s}".format(char+32))
+        if ((str.isupper(char))):
+            print("{}".format(char))
+        print("{:s}".format(char-32))
