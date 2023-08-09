1b5309362465409756bde72cbce2a932b8b3c5ab
RahmaRubi <rahmaelroby47@gmail.com>
2023-08-09 06:47:22 -0700

    done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,5 @@
 def uppercase(str):
     for char in str:
         if ((str.islower(char))):
-            print("{:s}".format(ord(char)-32))
-        print("{}".format(char))
+            print("{:s}".format(ord(char)-32, end=""))
+        print("{}".format(char), end="")
