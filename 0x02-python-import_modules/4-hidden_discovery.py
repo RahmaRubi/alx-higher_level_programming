cd2e2c05a2071e6f606d3e2f64555b36f0b0b121
RahmaRubi <rahmaelroby47@gmail.com>
2023-08-11 03:26:36 -0700

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     import hidden_4
     mod_names = dir(hidden_4)
     filterd_names = [name for name in mod_names if (not name.startswith("__"))]
-    for i in filterd_names:
-        print("{}".format(i))
+    for name in filterd_names:
+        print("{}".format(name))
