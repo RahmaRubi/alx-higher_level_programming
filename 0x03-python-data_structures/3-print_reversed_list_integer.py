3e50668167edcd535516ae435e4b2b1f0d45f52f
RahmaRubi <rahmaelroby47@gmail.com>
2023-08-13 21:47:08 -0700

    Done

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+#include "lists.h"
+def print_reversed_list_integer(my_list=[]):
+    if (my_list is None):
+        print("", end="")
+    else:
+        for i in my_list:
+            print("{:d}".format(my_list[-i]))
