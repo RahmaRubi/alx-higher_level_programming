abd8b63dc5596544a4a40d51d23480e87ca559ec
RahmaRubi <rahmaelroby47@gmail.com>
2023-09-06 06:35:48 -0700

    Done

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" class rectangle"""
+
+
+class Rectangle:
+    """ rectangle class"""
+    def __init__(self, width=0, height=0):
+        """ an instance method called when an object is created"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ width getter/setter function """
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height gettter/setter function"""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def area(self):
+        """ area getter """
+        return (self.width) * (self.height)
+    @property
+    def perimeter(self):
+        """ perimeter getter """
+        return ((self.width *2) + (self.height*2))
