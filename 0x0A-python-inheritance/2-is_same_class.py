5678497219b400fe308348f93f528c24e49f6669
RahmaRubi <rahmaelroby47@gmail.com>
2023-09-11 04:11:44 -0700

    betty

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-    """ mylist class """
-    def is_same_class(obj, a_class):
-        """ func checks if the object is instance for the class or not """
-        if type(obj) == a_class:
-            return (True)
-        else: 
-            return (False)
+""" mylist class """
+def is_same_class(obj, a_class):
+    """ func checks if the object is instance for the class or not """
+    if type(obj) == a_class:
+        return (True)
+    else:
+        return (False)
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-    """ is kind of a class """
-    def is_kind_of_class(obj, a_class):
-        """ returns True if the object is an instance of, or if the object is an instance of a class that inheri        ted from, the specified class ; otherwise False """
-        if (isinstance(obj, a_class)):
-            return (True)
-        else:
-            return (False)
+""" is kind of a class """
+def is_kind_of_class(obj, a_class):
+    """ returns True if the object is an instance of, or if the object is an instance of a class that inheri        ted from, the specified class ; otherwise False """
+    if (isinstance(obj, a_class)):
+        return (True)
+    else:
+        return (False)
