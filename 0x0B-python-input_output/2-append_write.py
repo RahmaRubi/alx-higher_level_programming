23ebaff9bfa6f5a744e0c66da0584c7e6f822d31
RahmaRubi <rahmaelroby47@gmail.com>
2023-09-12 12:19:11 -0700

    append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """ function appending text at the end """
     with open(filename, mode='a+', encoding='UTF8') as f:
-        return(f.write(text))
+        return(f.write(text), end="")
