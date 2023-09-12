c5da3d038de70c153f670f6856dae070e2b81846
RahmaRubi <rahmaelroby47@gmail.com>
2023-09-12 12:02:48 -0700

    docstring

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """ write function returns number of charchters """
     with open(filename, mode='w', encoding='UTF8') as f:
         return (f.write(text))
