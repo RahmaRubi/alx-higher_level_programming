05c272296aae5efe8d31304f9f9b528c5d0c3de4
RahmaRubi <rahmaelroby47@gmail.com>
2023-09-20 07:48:42 -0700

    hash

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" hash """
 from models.base import Base
 """ import from base """
 
@@ -10,59 +11,76 @@ class Rectangle(Base):
         """ init function """
 
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
-        @property
-        def width(self):
-            """ getter/ setter for width """
-            return(self.__width)
+    @property
+    def width(self):
+        """ getter/ setter for width """
+        return (self.__width)
 
-        @width.setter
-        def width(self, value):
-            if not isinstance(width, int):
-                raise TypeError("width must be an intger")
-            if width <= 0:
-                raise ValueError("width must be > 0")
-            self.__width = width
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an intger")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """ getter/ setter for height """
-            return(self.__height)
+    @property
+    def height(self):
+        """ getter/ setter for height """
+        return (self.__height)
 
-        @height.setter
-        def height(self, value):
-            if not isinstance(height, int):
-                raise TypeError("height must be an intger")
-            if height <= 0:
-                raise ValueError("height must be > 0")
-            self.__height = height
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an intger")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """ getter/ setter for x """
-            return(self.__x)
+    @property
+    def x(self):
+        """ getter/ setter for x """
+        return (self.__x)
 
-        @x.setter
-        def x(self, value):
-            if not isinstance(x, int):
-                raise TypeError("x must be an intger")
-            if x < 0:
-                raise ValueError("x must be > 0")
-            self.__x = x
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an intger")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """ getter/ setter for y """
-            return(self.__y)
+    @property
+    def y(self):
+        """ getter/ setter for y """
+        return (self.__y)
 
-        @y.setter
-        def y(self, value):
-            if not isinstance(y, int):
-                raise TypeError("y must be an intger")
-            if y < 0:
-                raise ValueError("y must be > 0")
-            self.__y = y
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an intger")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ area of the rectangle """
+        return (self.__width * self.__height)
+
+    def display(self):
+        """ disply rectangle represented by # """
+        rect = ""
+        symbol = "#"
+        pattern = symbol * self.width
+        for j in range(self.__height):
+            print(pattern)
+
+    def __str__(self):
+        """ a dunder function called implicitly when printing the object """
+        return "[{}] ({}) {}/{} - {}/{}".format(
+        type(self), self.id, self.__x, self.__y, self.__width, self.__height)
