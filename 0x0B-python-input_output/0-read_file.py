edb4d0caeee2fd5a2c1d944cc330a201e29b67b0
Rahma Rubi <rahmaelroby47@gmail.com>
2024-02-06 08:59:27 -0800

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ reading and printing function """
     with open(filename, 'r', encoding="utf-8") as f:
         content = f.read()
-        print(content)
+        print(content, end="")
