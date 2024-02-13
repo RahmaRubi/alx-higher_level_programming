02a37ed7689ee27961312afe63fac87452ec09f5
Rahma Rubi <rahmaelroby47@gmail.com>
2024-02-13 07:21:16 -0800

    all pushed

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,5 +32,42 @@ class Base:
                 list_dict = [obj.to_dictionary() for obj in list_objs]
                 list_str = Base.to_json_string(list_dict)
                 f.write(list_str)
-            
 
+    def from_json_string(json_string):
+        """ converting the json string json_string to its original 
+        data structure which is a list of dict
+        """
+        if json_string is None or "":
+            return("[]")
+        return(json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """that returns an instance with all attributes already set
+        """
+        obj = cls(1, 1)
+        if dictionary:
+            obj.update(**dictionary)
+        return (obj)
+            
+    
+    @classmethod
+    def load_from_file(cls):
+        """
+        returns a list of instances
+        """
+        filename = f"{cls.__name__}.json"
+
+        try:
+            with open(filename, 'r') as file:
+                json_data = file.read()
+        except FileNotFoundError:
+            return []
+
+        json_rep_list = cls.from_json_string(json_data)
+        newlist = list()
+        for dic in json_rep_list:
+            obj = cls.create(**dic)
+            # astreisk for **dictionary in function def
+            newlist.append(obj)
+        return newlist
