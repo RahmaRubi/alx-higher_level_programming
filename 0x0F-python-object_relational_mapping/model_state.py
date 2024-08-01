4af3127b4de242db910f4d0ebb5a1611faa9b8d4
root <root@DESKTOP-OB6IUIJ>
2024-08-02 00:08:33 +0300

    states not state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,6 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 class State(Base):
     """class state for creating table state"""
-    __tablename__= 'state'
+    __tablename__= 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
